0e3de0d748691cd9b1056648bad17709bbc55ce1
Konstantin_Romanov <romanov.konstantin.il@gmail.com>
2018-09-30 19:41:03 +0300

    Lambda part 1 exercise 1 has done

--- src/test/java/lambda/part1/exercise/Exercise1.java
+++ src/test/java/lambda/part1/exercise/Exercise1.java
@@ -1,10 +1,13 @@
 package lambda.part1.exercise;
 
+import com.google.common.collect.FluentIterable;
 import lambda.data.Person;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -17,6 +20,18 @@ class Exercise1 {
         Person[] persons = getPersons();
 
         // TODO use Arrays.sort
+        class PersonComparator implements Comparator<Person> {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if (o1 == null && o2 == null) return 0;
+                if (o1 != null && o2 == null) return 1;
+                if (o1 == null) return -1;
+
+                return o1.getAge() - o2.getAge();
+            }
+        }
+
+        Arrays.sort(persons, new PersonComparator());
 
         assertThat(persons, is(arrayContaining(
                 new Person("Иван", "Мельников", 20),
@@ -31,6 +46,18 @@ class Exercise1 {
         Person[] persons = getPersons();
 
         // TODO use Arrays.sort
+        Comparator<Person> personComparator = new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if (o1 == null && o2 == null) return 0;
+                if (o1 != null && o2 == null) return 1;
+                if (o1 == null) return -1;
+
+                return o1.getAge() - o2.getAge();
+            }
+        };
+
+        Arrays.sort(persons, personComparator);
 
         assertThat(persons, is(arrayContaining(
                 new Person("Иван", "Мельников", 20),
@@ -45,7 +72,10 @@ class Exercise1 {
         Person[] persons = getPersons();
 
         // TODO use Arrays.sort
+        Comparator<Person> personByLastNameComparator = (p1, p2) -> p1.getLastName().compareToIgnoreCase(p2.getFirstName());
+        Comparator<Person> personByFirstNameComparator = (p1, p2) -> p1.getFirstName().compareToIgnoreCase(p2.getFirstName());
 
+        Arrays.sort(persons, personByLastNameComparator.thenComparing(personByFirstNameComparator));
 
         assertThat(persons, is(arrayContaining(
                 new Person("Алексей", "Доренко", 40),
@@ -60,7 +90,16 @@ class Exercise1 {
         List<Person> persons = Arrays.asList(getPersons());
 
         // TODO use FluentIterable
-        Person person = null;
+        Predicate<Person> personIs30yoPredicate = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getAge() == 30;
+            }
+        };
+
+        Person person = FluentIterable.from(persons)
+                            .firstMatch(personIs30yoPredicate::test)
+                            .orNull();
 
         assertThat(person, is(new Person("Николай", "Зимов", 30)));
     }
@@ -70,7 +109,14 @@ class Exercise1 {
         List<Person> persons = Arrays.asList(getPersons());
 
         // TODO use FluentIterable
-        Person person = null;
+        Person person = FluentIterable.from(persons)
+                .firstMatch(new com.google.common.base.Predicate<Person>() {
+                    @Override
+                    public boolean apply(Person person) {
+                        return person.getAge() == 30;
+                    }
+                })
+                .orNull();
 
         assertThat(person, is(new Person("Николай", "Зимов", 30)));
     }
