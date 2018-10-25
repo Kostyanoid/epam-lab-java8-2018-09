22fa4de415a5e7857308e0272bbe9574bc3fced7
Konstantin Romanov <konstantin_romanov@epam.com>
2018-10-25 12:30:28 +0300

    Streams part 2 exercise 3 has done

--- src/test/java/streams/part2/exercise/Exercise3.java
+++ src/test/java/streams/part2/exercise/Exercise3.java
@@ -2,6 +2,17 @@ package streams.part2.exercise;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -12,7 +23,11 @@ class Exercise3 {
     void createLimitedStringWithOddNumbersSeparatedBySpaces() {
         int countNumbers = 10;
 
-        String result = null;
+        String result = Stream.iterate(1, i -> i + 1)
+                .filter(isOddNumber())
+                .limit(countNumbers)
+                .map(String::valueOf)
+                .collect(joining(" "));
 
         assertThat(result, is("1 3 5 7 9 11 13 15 17 19"));
     }
@@ -21,8 +36,55 @@ class Exercise3 {
     void extractEvenNumberedCharactersToNewString() {
         String source = "abcdefghijklm";
 
-        String result = null;
+        String result = Stream.iterate(0, i -> i + 1)
+                .limit(source.length())
+                .filter(isEvenNumber())
+                .map(source::charAt)
+                .collect(new CharJoining());
 
         assertThat(result, is("acegikm"));
+    }
+
+    private static class CharJoining implements Collector<Character, StringBuffer, String> {
+
+        private HashSet<Characteristics> characteristics;
+
+        public CharJoining() {
+            characteristics = new HashSet<>();
+            characteristics.add(Characteristics.CONCURRENT);
+        }
+
+        @Override
+        public Supplier<StringBuffer> supplier() {
+            return StringBuffer::new;
+        }
+
+        @Override
+        public BiConsumer<StringBuffer, Character> accumulator() {
+            return StringBuffer::append;
+        }
+
+        @Override
+        public BinaryOperator<StringBuffer> combiner() {
+            return StringBuffer::append;
+        }
+
+        @Override
+        public Function<StringBuffer, String> finisher() {
+            return StringBuffer::toString;
+        }
+
+        @Override
+        public Set<Characteristics> characteristics() {
+            return characteristics;
+        }
+    }
+
+    private static Predicate<Integer> isOddNumber() {
+        return n -> n % 2 == 1;
+    }
+
+    private static Predicate<Integer> isEvenNumber() {
+        return n -> n % 2 == 0;
     }
 }
